e09caddf09c8046ac9a129ddde5b4544dd436de8
nathan evans <48648779+nkevan01@users.noreply.github.com>
2019-08-09 19:01:58 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Pop from './components/Pop';
+import Footer from './components/Footer.js';
+import './style.css';
+
+
+function App(){
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Pop />
+            <Footer />
+        </div>
+    )
+};
+
+
+
+export default App;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import Header from './components/Header'
+import Main from './components/Main'
+import Pop from './components/Pop'
+import Footer from './components/Footer.js'
+ 
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
